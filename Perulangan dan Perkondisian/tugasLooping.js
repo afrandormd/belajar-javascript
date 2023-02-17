9cc02cd498e6c700c95eb2815a3c0086e41d582a
afrandormd <afrandormd@gmail.com>
2023-02-17 19:35:59 +0700

    menambahkan file tugasLooping.js

---
+++ Perulangan dan Perkondisian/tugasLooping.js
@@ -0,0 +1,20 @@
+// tugas by Dea Afrizal
+
+// for loop
+for (let i = 1; i < 20; i++) {
+    if (i % 2 !== 0) console.log(`${i} adalah bilangan ganjil`)
+}
+
+// convert ke while
+let j = 20
+while (j > 1) {
+    if (j % 2 !== 0) console.log(`${j} adalah bilangan ganjil`)
+j--
+}
+
+// do while loop
+let x = 1
+do {
+    console.log('Hallo semuanya')
+x++
+} while (x < 10)
